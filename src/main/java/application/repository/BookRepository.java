e346df23ad52f8af547a9de0645d8f3bd86408d4
Anish Sana <anish@280first.com>
2018-09-08 19:50:34 -0400

    Added search by author and title

--- src/main/java/application/repository/BookRepository.java
+++ src/main/java/application/repository/BookRepository.java
@@ -1,5 +1,7 @@
 package application.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -10,5 +12,9 @@ import application.model.Book;
 public interface BookRepository extends MongoRepository<Book, String> {
 
 	Book findBookByIsbn(@Param("isbn") String isbn);
+	
+	Book findBookByTitle(@Param("title") String title);
+	
+	List<Book> findBooksByAuthor(@Param("author") String author);
 	
 }
